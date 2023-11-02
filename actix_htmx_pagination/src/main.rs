0afb11fbce77f56d1c4e5e0525531c2175acb5d1
Juan A <140235896+juanaw6@users.noreply.github.com>
2023-11-03 03:12:04 +0700

    test: Add logger

--- actix_htmx_pagination/src/main.rs
+++ actix_htmx_pagination/src/main.rs
@@ -1,6 +1,8 @@
-use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use actix_web::{web, App, HttpServer, HttpResponse, Responder, Error};
 use serde::{Serialize, Deserialize};
 use std::sync::Mutex;
+use env_logger::Env;
+use log::{info, error};
 
 #[derive(Serialize, Clone)]
 struct Item {
@@ -25,13 +27,15 @@ async fn main() -> std::io::Result<()> {
         current_page: Mutex::new(-1)
     });
 
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    info!("Starting the server at http://127.0.0.1:8080/");
     HttpServer::new(move || {
         App::new()
             .app_data(app_data.clone())
             .route("/", web::get().to(index))
             .route("/items", web::post().to(paginated_items))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
@@ -45,7 +49,13 @@ fn paginate(items: &[Item], page: usize) -> Vec<Item> {
 }
 
 async fn paginated_items(data: web::Data<AppState>, action: web::Form<Action>) -> impl Responder {
-    let mut page = data.current_page.lock().unwrap();
+    let mut page = match data.current_page.lock() {
+        Ok(guard) => guard,
+        Err(e) => {
+            error!("Failed to acquire lock on current_page: {}", e);
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
     match action.direction.as_str() {
         "next" => *page += 1,
         "prev" => *page -= 1,
@@ -54,7 +64,7 @@ async fn paginated_items(data: web::Data<AppState>, action: web::Form<Action>) -
     let total_items = data.items.len();
     let total_pages = (total_items + PAGE_SIZE - 1) / PAGE_SIZE;
     let items = paginate(&data.items, (*page).try_into().unwrap());
-    println!("Current page: {}", *page);
+    info!("Serving paginated items for page {}", *page);
     
     // Generate the HTML for the paginated items
     let items_html = items.iter().map(|item| {
@@ -68,7 +78,7 @@ async fn paginated_items(data: web::Data<AppState>, action: web::Form<Action>) -
         r#"<button hx-post="/items" hx-target='#items-list' hx-vals='{"direction": "prev"}' hx-swap="innerHTML">Previous</button>"#.to_string()
     };
 
-    let next_button_html = if *page == total_pages as i32 {
+    let next_button_html = if *page == (total_pages as i32) - 1 {
         r#"<button disabled>Next</button>"#.to_string()
     } else {
         r#"<button hx-post="/items" hx-target='#items-list' hx-vals='{"direction": "next"}' hx-swap="innerHTML">Next</button>"#.to_string()
@@ -84,9 +94,9 @@ async fn paginated_items(data: web::Data<AppState>, action: web::Form<Action>) -
     HttpResponse::Ok().content_type("text/html").body(full_html)
 }
 
-
-
-
-async fn index() -> impl Responder {
+async fn index(data: web::Data<AppState>) -> impl Responder {
+    let mut page = data.current_page.lock().unwrap();
+    *page = -1;
+    info!("Reset pagination and serving index.html");
     HttpResponse::Ok().content_type("text/html").body(include_str!("../static/index.html"))
 }
