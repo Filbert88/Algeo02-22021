fa073ea171a3054d038a2bafdd0cec9617be4852
RealAzzmi <azmibazeid@gmail.com>
2023-11-04 13:05:31 +0700

    feat: Prototype nonuniform range in global CBIR

--- program/main.py
+++ program/main.py
@@ -1,19 +1,29 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 import math
 
-def get_feature_vector(image_location, h_bins, s_bins, v_bins):
+def get_global_color_vector(image_location):
 
     img = cv2.imread(image_location)
     height, width, depth = img.shape
 
-    hsv_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # hist = cv2.calcHist([hsv_image], [0, 1, 2], None, [180, 256, 256], [0., 180., 0., 256., 0., 256.])
+    h_ranges = [(0, 59), (60, 119), (120, 180)]
+    s_ranges = [(0, 84), (85, 169), (170, 255)]
+    v_ranges = [(0, 84), (85, 169), (170, 255)]
 
-    hist = cv2.calcHist([hsv_image], [0, 1, 2], None, [h_bins, s_bins, v_bins], [0., 180., 0., 256., 0., 256.])
+    r_masks = [(r[0] <= image[:, :, 0]) & (image[:, :, 0] <= r[1]) for r in h_ranges]
+    g_masks = [(g[0] <= image[:, :, 1]) & (image[:, :, 1] <= g[1]) for g in s_ranges]
+    b_masks = [(b[0] <= image[:, :, 2]) & (image[:, :, 2] <= b[1]) for b in v_ranges]
 
-    return hist
+    partitions = [(rm & gm & bm) for rm in r_masks for gm in g_masks for bm in b_masks]
+    
+    color_vector = [np.sum(partition) for partition in partitions]
+    
+    return color_vector
+
 
 def cosine_similarity(vec1, vec2, h_bins=-1, s_bins=-1, v_bins=-1):
     return np.sum(vec1 * vec2) / (np.sqrt(np.sum(vec1 * vec1)) * np.sqrt(np.sum(vec2 * vec2)))
@@ -30,10 +40,11 @@ def cosine_similarity(vec1, vec2, h_bins=-1, s_bins=-1, v_bins=-1):
     # return dot_product / (math.sqrt(a) * math.sqrt(b))
 
 if __name__ == "__main__":
-    h_bins = 30
-    s_bins = 30
-    v_bins = 30
-    vec1 = get_feature_vector("/home/azzmi/projects/Algeo02-22021/program/apples/apple01.png", h_bins, s_bins, v_bins)
-    vec2 = get_feature_vector("/home/azzmi/projects/Algeo02-22021/program/apples/apple13.png", h_bins, s_bins, v_bins)
-
-    print(cosine_similarity(vec1, vec2))
+    h_bins = 20
+    s_bins = 5
+    v_bins = 5
+    vec1 = get_global_color_vector("/home/azzmi/projects/Algeo02-22021/program/rainbow/pic01.png", h_bins, s_bins, v_bins)
+    # vec1 = get_global_color_vector("/home/azzmi/projects/Algeo02-22021/program/apples/apple01.png", h_bins, s_bins, v_bins)
+    # vec2 = get_feature_vector("/home/azzmi/projects/Algeo02-22021/program/apples/apple09.png", h_bins, s_bins, v_bins)
+    # print(np.sum(vec1))
+    # print(cosine_similarity(vec1, vec2))
