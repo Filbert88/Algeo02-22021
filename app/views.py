3c51c3aa646b19ceee56fb8dae45aefd2af9c9a0
Juan A <140235896+juanaw6@users.noreply.github.com>
2023-11-05 16:40:39 +0700

    feat: Add upload image

--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
-from flask import render_template
 from app import app
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask, request, render_template, redirect, url_for
 
 @app.route('/')
 def index():
+    os.makedirs('data/img/user_img', exist_ok=True)
+    os.makedirs('data/img/dataset', exist_ok=True)
     return render_template('index.html')
 
 @app.route('/developers')
@@ -16,3 +20,20 @@ def guides():
 @app.route('/program')
 def program():
     return render_template('program.html')
+
+@app.route('/upload_image', methods=['POST'])
+def upload_image() :
+    if 'image' not in request.files:
+        return redirect(request.url)
+    file = request.files['image']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = secure_filename(file.filename)
+        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(save_path)
+        return "<h1>File uploaded successfully.</h1>"
+    
+@app.route('/reset_message', methods=['GET'])
+def reset_message() :
+    return ""
