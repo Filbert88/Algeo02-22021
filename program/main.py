989eaeb76ba9bea07e7cd2c07a6f7ba24dbd2a67
Juan A <140235896+juanaw6@users.noreply.github.com>
2023-11-05 13:42:13 +0700

    feat: Add launch app animation

--- program/main.py
+++ program/main.py
@@ -1,12 +1,13 @@
 from color_cbir import *
 
 if __name__ == "__main__":
-    image_1 = load_image_as_hsv("/home/azzmi/projects/Algeo02-22021/program/apples/apple11.png")
+    image_1 = load_image_as_hsv("apples/apple11.png")
     vector_1 = get_vector(image_1)
+    print(vector_1)
 
-
-    image_2 = load_image_as_hsv("/home/azzmi/projects/Algeo02-22021/program/apples/apple13.png")
+    image_2 = load_image_as_hsv("rainbow/pic02.png")
     vector_2 = get_vector(image_2)
+    print(vector_2)
 
     similarity = cosine_similarity(vector_1, vector_2)
     print(similarity)
