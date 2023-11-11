aa32da08784cdfaa9c10a9fea8332485a4528d0d
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-11-12 02:17:55 +0700

    feat: Add styling for upload zip and folder (80%)

---
+++ app/static/handlefile.js
@@ -0,0 +1,21 @@
+function updateFileName() {
+  var input = document.getElementById("zipfile");
+  var fileNameDisplay = document.getElementById("zip-file-name");
+  if (input.files && input.files.length > 0) {
+    var fileName = input.files[0].name; 
+    fileNameDisplay.textContent = fileName; 
+  } else {
+    fileNameDisplay.textContent = "No file chosen"; 
+  }
+}
+
+function updateFolderName() {
+  var folderInput = document.getElementById('folder');
+  var folderNameDisplay = document.getElementById('folder-name');
+  if (folderInput.files.length > 0) {
+    folderNameDisplay.textContent = 'Folder selected';
+  } else {
+    folderNameDisplay.textContent = 'No folder chosen';
+  }
+}
+
