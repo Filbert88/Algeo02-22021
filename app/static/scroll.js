39703ee9d31c1d813a5a25b8bc48dd4d7d81a917
Juan A <140235896+juanaw6@users.noreply.github.com>
2023-11-19 15:57:18 +0700

    style: Restructure repo

--- app/static/scroll.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function () {
-  var header = document.querySelector(".header");
-  if (window.scrollY > 100) {
-    header.classList.add("shrink");
-  } else {
-    header.classList.remove("shrink");
-  }
-};
--- program/rename.sh
+++
@@ -1,5 +0,0 @@
-for f in *.jpg ; do
-  if [[ $(file -b --mime-type "$f") = image/png ]] ; then
-    mv "$f" "${f/%.png/.jpg}"
-  fi
-done
